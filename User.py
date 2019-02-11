12ef04997c26c68d72ba7ac908a8eff503ab1bcc
mukantwarivirginie <virgm2020@gmail.com>
2019-02-11 16:22:18 +0200

    import User

--- User.py
+++ User.py
@@ -2,15 +2,17 @@ class User:
     """
     Class that generates new instances of users.
     """
+    User_list = [] # Empty contact list
 
-    User_list = [] 
+    def __init__(self,first_name,last_name,number,password,email):
 
- def __init__(self,first_name,last_name,number,email,PassWord):
+      # docstring removed for simplicity
 
         self.first_name = first_name
         self.last_name = last_name
         self.phone_number = number
+        self.password = password
         self.email = email
-        self.PassWord = PassWord
+   
 
-
+   
---
+++ user-text.py
@@ -0,0 +1,3 @@
+import unittest # Importing the unittest modulefrom 
+from user import User # Importing the User class
+class TestUser(unittest.TestCase):
