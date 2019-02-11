c2a6bf05b59369761b5b5ae9587358297a415d92
mukantwarivirginie <virgm2020@gmail.com>
2019-02-11 17:09:00 +0200

    added some in user-text.py

--- user-text.py
+++ user-text.py
@@ -1,3 +1,32 @@
-import unittest # Importing the unittest modulefrom 
-from user import User # Importing the User class
+import unittest 
+from user import User 
 class TestUser(unittest.TestCase):
+   '''
+    Test class that defines test cases for the User class behaviours.
+
+    Args:
+        unittest.TestCase: TestCase class that helps in creating test cases
+    '''# Items up here .......
+
+    def setUp(self):
+        '''
+        Set up method to run before each test cases.
+        '''
+        self.new_User = User("virginie","mukantwari","0786679517","pasword","virgm2020@gmail.com",) # create User object
+
+
+    def test_init(self):
+        '''
+        test_init test case to test if the object is initialized properly
+        '''
+
+        self.assertEqual(self.new_User.first_name,"virginie")
+        self.assertEqual(self.new_User.last_name,"mukantwari")
+        self.assertEqual(self.new_User.phone_number,"0786679517")
+        self.assertEqual(self.new_User.pasword,"iradukunda")
+        self.assertEqual(self.new_User.email,"virgm2020@gmail.com")
+        
+
+
+if __name__ == '__main__':
+    unittest.main()
