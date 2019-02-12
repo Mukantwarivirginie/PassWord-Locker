2bc1d26a987c20538314e73e3fc233eab143206c
mukantwarivirginie <virgm2020@gmail.com>
2019-02-12 15:38:20 +0200

    added save user

--- run.py
+++ run.py
@@ -1,1 +1,13 @@
 from User import User
+def create_user(self,first_name,last_name,number,password,email):
+    '''
+    Function to create a new contact
+    '''
+    new_user = User(self,first_name,last_name,number,password,email)
+    return new_user
+
+    def save_contacts(user):
+    '''
+    Function to save user
+    '''
+    user.save_user()
